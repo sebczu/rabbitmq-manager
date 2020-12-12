df4e594b23e17df4bdc4b73abc064a7804ef9ce3
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2020-12-12 19:11:11 +0100

    configuration

--- service/init.sh
+++ service/init.sh
@@ -20,7 +20,17 @@ do
   sleep 2
 done
 
-rabbitmqadmin --host=rabbitmq --port=15672 --username=admin --password=test list connections
 
-echo "stop"
+rabbitmqadmin --host=$RABBITMQ_HOSTNAME --port=$RABBITMQ_PORT --username=$RABBITMQ_USERNAME --password=$RABBITMQ_PASSWORD import /rabbitmq_config/queue_test.json
+rabbitmqadmin --host=$RABBITMQ_HOSTNAME --port=$RABBITMQ_PORT --username=$RABBITMQ_USERNAME --password=$RABBITMQ_PASSWORD import /rabbitmq_config/queue_test2.json
+rabbitmqadmin --host=$RABBITMQ_HOSTNAME --port=$RABBITMQ_PORT --username=$RABBITMQ_USERNAME --password=$RABBITMQ_PASSWORD import /rabbitmq_config/exchange_test.json
+rabbitmqadmin --host=$RABBITMQ_HOSTNAME --port=$RABBITMQ_PORT --username=$RABBITMQ_USERNAME --password=$RABBITMQ_PASSWORD import /rabbitmq_config/binding_test.json
+rabbitmqadmin --host=$RABBITMQ_HOSTNAME --port=$RABBITMQ_PORT --username=$RABBITMQ_USERNAME --password=$RABBITMQ_PASSWORD publish routing_key="" exchange=test payload="test_message"
+
+echo "actual configuration:"
+rabbitmqadmin --host=$RABBITMQ_HOSTNAME --port=$RABBITMQ_PORT --username=$RABBITMQ_USERNAME --password=$RABBITMQ_PASSWORD export rabbit_conifg
+cat rabbit_conifg
+echo ""
+
+echo "finish"
 exit 0
